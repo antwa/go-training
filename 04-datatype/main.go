e703c1defaed4592573fc3e696d249ff0a1d3346
antwa <antwa1988@gmail.com>
2023-05-19 09:12:52 +0700

    add variable train

---
+++ 04-datatype/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// varible
+	var varInt int = 0
+	var varFloat float32 = 0
+	var varString string = "string"
+	var varBool bool = false
+
+	// declare multi variable
+	var a, b, c, d, e, f int = 1, 2, 3, 4, 5, 6
+
+}
